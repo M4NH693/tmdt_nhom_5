9bd769aaf43b59f9fac56b4d1c7904bf2d4e28c5
Nguyen Van Manh <vmanhsaber119@gmail.com>
2026-04-05 22:51:51 +0700

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -3,7 +3,7 @@
  * Database Configuration
  */
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'failover');
+define('DB_NAME', 'bookstore');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_CHARSET', 'utf8mb4');
